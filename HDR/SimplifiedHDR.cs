611db6085b2b152b97f18d8ae412ddb00f16850f
porry20009 <376175018@qq.com>
2016-08-21 20:08:15 +0800

    HDR

---
+++ HDR/SimplifiedHDR.cs
@@ -0,0 +1,241 @@
+﻿using UnityEngine;
+public class SimplifiedHDR : MonoBehaviour
+{
+    enum HDRPass
+    {
+        Luminance = 0,
+        Average4x4Samples = 1,
+        Average2x2Samples = 2,
+        AdaptiveLuminance = 3,
+        AutoExposureWithoutBloom = 4,
+        AutoExposureWithBloom = 5,
+        Brightness = 6
+    }
+
+    enum GaussianBlurPass
+    {
+        Horizontal3x3 = 0,
+        Vertical3x3 = 1,
+        Horizontal5x5 = 2,
+        Vertical5x5 = 3
+    }
+
+    enum HDRProcessmap
+    {
+        PreLuminance = 0,
+        CurrLuminance = 1,
+        NewLuminance = 2,
+        DownScale128x128 = 3,
+        DownScale64x64 = 4,
+        DownScale16x16 = 5,
+        DownScale4x4 = 6
+    }
+
+    public float mAdaptiveSpeed = 1.0f;
+    [Range(0, 1)]
+    public float mMiddleGray = 0.5f;//指定图像base亮度
+    public float mExposure = 2.0f;
+    public float mCoeff0 = 2.0f;
+
+    public bool mIsBloomDebug = false;
+    public bool mIsBloom = true;
+    public float mBrightOffset = -0.7f;
+    public float mBrightScale = 0.6f;
+
+    Material mHDRMaterial = null;
+    Material mGaussianMaterial = null;
+    RenderTexture[] mHDRProcessmap = { null, null, null, null, null, null, null };
+
+    void Awake()
+    {
+        CreateMaterial();
+        CreateProcessMap();
+    }
+
+    void OnDestroy()
+    {
+        for (int i = 0; i < mHDRProcessmap.Length; i++)
+        {
+            if (mHDRProcessmap[i] != null)
+            {
+                Destroy(mHDRProcessmap[i]);
+                mHDRProcessmap[i] = null;
+            }
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        RGBToLuminance(src);
+        AverageLuminance();
+        AdaptiveLuminance();
+        if (mIsBloom)
+        {
+            AutoExposureWithBloom(src, dest);
+        }
+        else
+        {
+            AutoExposureWithoutBloom(src, dest);
+        }
+    }
+
+    void CreateMaterial()
+    {
+        mHDRMaterial = new Material(Shader.Find("PostEffect/SimplifiedHDR"));
+        if (mHDRMaterial == null)
+        {
+            Debug.LogWarning("SimplifiedHDR.shader丢失!");
+            enabled = false;
+        }
+
+        mGaussianMaterial = new Material(Shader.Find("PostEffect/GaussianBlur"));
+        if (mGaussianMaterial == null)
+        {
+            Debug.LogWarning("GaussianBlur.shader丢失!");
+            enabled = false;
+        }
+    }
+
+    void CreateProcessMap()
+    {
+        RenderTextureFormat format = RenderTextureFormat.ARGB32;
+        mHDRProcessmap[(int)HDRProcessmap.DownScale128x128] = new RenderTexture(128, 128, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.DownScale64x64] = new RenderTexture(64, 64, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.DownScale16x16] = new RenderTexture(16, 16, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.DownScale4x4] = new RenderTexture(4, 4, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.CurrLuminance] = new RenderTexture(1, 1, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.PreLuminance] = new RenderTexture(1, 1, 0, format);
+        mHDRProcessmap[(int)HDRProcessmap.NewLuminance] = new RenderTexture(1, 1, 0, format);
+
+        for (int i = 0; i < mHDRProcessmap.Length; i++)
+        {
+            mHDRProcessmap[i].generateMips = false;
+            mHDRProcessmap[i].filterMode = FilterMode.Point;
+            mHDRProcessmap[i].wrapMode = TextureWrapMode.Clamp;
+
+            RenderTexture mainRTT = RenderTexture.active;
+            RenderTexture.active = mHDRProcessmap[i];
+            GL.Clear(false, true, Color.black);
+            RenderTexture.active = mainRTT;
+        }
+    }
+
+
+    void RGBToLuminance(RenderTexture src)
+    {
+        mHDRMaterial.SetTexture("_MainTex", src);
+        mHDRProcessmap[(int)HDRProcessmap.DownScale128x128].DiscardContents();
+        Graphics.Blit(src, mHDRProcessmap[(int)HDRProcessmap.DownScale128x128], mHDRMaterial, (int)HDRPass.Luminance);
+    }
+
+    void AverageLuminance()
+    {
+        LumDownScale2x2(mHDRProcessmap[(int)HDRProcessmap.DownScale128x128], mHDRProcessmap[(int)HDRProcessmap.DownScale64x64], new Vector2(1.0f / 128.0f, 1.0f / 128.0f));
+        LumDownScale4x4(mHDRProcessmap[(int)HDRProcessmap.DownScale64x64], mHDRProcessmap[(int)HDRProcessmap.DownScale16x16], new Vector2(1.0f / 64.0f, 1.0f / 64.0f));
+        LumDownScale4x4(mHDRProcessmap[(int)HDRProcessmap.DownScale16x16], mHDRProcessmap[(int)HDRProcessmap.DownScale4x4], new Vector2(1.0f / 16.0f, 1.0f / 16.0f));
+        LumDownScale4x4(mHDRProcessmap[(int)HDRProcessmap.DownScale4x4], mHDRProcessmap[(int)HDRProcessmap.CurrLuminance], new Vector2(1.0f / 4.0f, 1.0f / 4.0f));
+    }
+
+    bool mFirst = true;
+    void AdaptiveLuminance()
+    {
+        if (mFirst)
+        {
+            Graphics.Blit(mHDRProcessmap[(int)HDRProcessmap.CurrLuminance], mHDRProcessmap[(int)HDRProcessmap.NewLuminance]);
+            mFirst = false;
+        }
+        else
+        {
+            mHDRMaterial.SetTexture("_CurrAverLumTex", mHDRProcessmap[(int)HDRProcessmap.CurrLuminance]);
+            mHDRMaterial.SetTexture("_PreAverLumTex", mHDRProcessmap[(int)HDRProcessmap.PreLuminance]);
+            mHDRMaterial.SetFloat("_AdaptiveSpeed", mAdaptiveSpeed);
+            mHDRMaterial.SetFloat("_ElapsedTime", Time.deltaTime);
+            mHDRProcessmap[(int)HDRProcessmap.NewLuminance].DiscardContents();
+            Graphics.Blit(null, mHDRProcessmap[(int)HDRProcessmap.NewLuminance], mHDRMaterial, (int)HDRPass.AdaptiveLuminance);
+        }
+    }
+
+    void AutoExposureWithoutBloom(RenderTexture src, RenderTexture dest)
+    {
+        mHDRMaterial.SetTexture("_MainTex", src);
+        mHDRMaterial.SetTexture("_AdaptedLumTex", mHDRProcessmap[(int)HDRProcessmap.NewLuminance]);
+        mHDRMaterial.SetFloat("_MiddleGray", mMiddleGray);
+        mHDRMaterial.SetFloat("_Exposure", mExposure);
+        mHDRMaterial.SetFloat("_Coeff0", mCoeff0);
+        Graphics.Blit(src, dest, mHDRMaterial, (int)HDRPass.AutoExposureWithoutBloom);
+        SwapRenderTexture((int)HDRProcessmap.NewLuminance, (int)HDRProcessmap.PreLuminance);
+    }
+
+    void SwapRenderTexture(int a, int b)
+    {
+        RenderTexture t = mHDRProcessmap[a];
+        mHDRProcessmap[a] = mHDRProcessmap[b];
+        mHDRProcessmap[b] = t;
+    }
+
+    //缩小4倍（采样16个点）
+    void LumDownScale4x4(RenderTexture src, RenderTexture dest, Vector2 texsizeInverse)
+    {
+        mHDRMaterial.SetTexture("_MainTex", src);
+        mHDRMaterial.SetVector("_AverImageSizeInverse", texsizeInverse);
+
+        dest.DiscardContents();
+        Graphics.Blit(src, dest, mHDRMaterial, (int)HDRPass.Average4x4Samples);
+    }
+
+    //缩小2倍（采样4个点）
+    void LumDownScale2x2(RenderTexture src, RenderTexture dest, Vector2 texsizeInverse)
+    {
+        mHDRMaterial.SetTexture("_MainTex", src);
+        mHDRMaterial.SetVector("_AverImageSizeInverse", texsizeInverse);
+
+        dest.DiscardContents();
+        Graphics.Blit(src, dest, mHDRMaterial, (int)HDRPass.Average2x2Samples);
+    }
+
+    void AutoExposureWithBloom(RenderTexture src, RenderTexture dest)
+    {
+        int scaleW = Screen.width / 4;
+        int scaleH = Screen.height / 4;
+        RenderTexture brightnessScaleTex = RenderTexture.GetTemporary(scaleW, scaleH, 0, RenderTextureFormat.ARGB32);
+        RenderTexture temp = RenderTexture.GetTemporary(scaleW, scaleH, 0, RenderTextureFormat.ARGB32);
+
+        //Brightness
+        mHDRMaterial.SetTexture("_CurrAverLumTex", mHDRProcessmap[(int)HDRProcessmap.NewLuminance]);
+        mHDRMaterial.SetFloat("_MiddleGray", mMiddleGray);
+        mHDRMaterial.SetFloat("_BrightOffset", mBrightOffset);
+        mHDRMaterial.SetFloat("_BrightScale", mBrightScale);
+        mHDRMaterial.SetFloat("_Exposure", mExposure);
+        mHDRMaterial.SetFloat("_Coeff0", mCoeff0);
+        Graphics.Blit(src, brightnessScaleTex, mHDRMaterial, (int)HDRPass.Brightness);
+
+        //Gaussian Blur
+        mGaussianMaterial.SetVector("_BlurTexSizeInverse", new Vector4(1.0f / (float)scaleW, 1.0f / (float)scaleH, 0, 0));
+        temp.DiscardContents();
+        Graphics.Blit(brightnessScaleTex, temp, mGaussianMaterial, (int)GaussianBlurPass.Horizontal5x5);
+
+        if (mIsBloomDebug)
+        {
+            brightnessScaleTex.DiscardContents();
+            Graphics.Blit(temp, brightnessScaleTex, mGaussianMaterial, (int)GaussianBlurPass.Vertical5x5);
+            Graphics.Blit(brightnessScaleTex, dest);
+        }
+        else
+        {
+            brightnessScaleTex.DiscardContents();
+            Graphics.Blit(temp, brightnessScaleTex, mGaussianMaterial, (int)GaussianBlurPass.Vertical5x5);
+            //Blend Screen And Brightness
+            mHDRMaterial.SetTexture("_MainTex", src);
+            mHDRMaterial.SetTexture("_BloomTex", brightnessScaleTex);
+            mHDRMaterial.SetTexture("_AdaptedLumTex", mHDRProcessmap[(int)HDRProcessmap.NewLuminance]);
+            mHDRMaterial.SetFloat("_MiddleGray", mMiddleGray);
+            mHDRMaterial.SetFloat("_Exposure", mExposure);
+            mHDRMaterial.SetFloat("_Coeff0", mCoeff0);
+            Graphics.Blit(src, dest, mHDRMaterial, (int)HDRPass.AutoExposureWithBloom);
+            SwapRenderTexture((int)HDRProcessmap.NewLuminance, (int)HDRProcessmap.PreLuminance);
+        }
+        RenderTexture.ReleaseTemporary(brightnessScaleTex);
+        RenderTexture.ReleaseTemporary(temp);
+    }
+}
+
---
+++ HDR/SimplifiedHDR.shader
@@ -0,0 +1,266 @@
+﻿Shader "PostEffect/SimplifiedHDR"
+{
+	CGINCLUDE
+	#include "UnityCG.cginc"
+            
+    uniform sampler2D _MainTex;
+	uniform sampler2D _CurrAverLumTex;
+	uniform sampler2D _PreAverLumTex;
+	uniform sampler2D _AdaptedLumTex;
+	uniform sampler2D _BloomTex;
+	
+	uniform half _MiddleGray;
+	uniform half _AdaptiveSpeed;
+	uniform half _ElapsedTime;
+	uniform half2 _AverImageSizeInverse;
+
+	uniform half _BrightOffset;
+	uniform half _BrightScale;
+	
+	uniform half _Exposure;
+	uniform half _Coeff0;
+
+	static const float2 g_texOffset4x4[16] = 
+	{
+		float2(0,0), float2(0,1), float2(0,2), float2(0,3),
+		float2(1,0), float2(1,1), float2(1,2), float2(1,3),
+		float2(2,0), float2(2,1), float2(2,2), float2(2,3),
+		float2(3,0), float2(3,1), float2(3,2), float2(3,3)
+	};
+
+	static const float2 g_texOffset2x2[4] = 
+	{
+		float2(0,0), float2(0,1),
+		float2(1,0), float2(1,1)
+	};
+
+    struct VS_OUTPUT
+    {
+        float4 pos : SV_POSITION;
+        half2 uv : TEXCOORD0;
+    };
+    
+    VS_OUTPUT MainVS(appdata_base input)
+    {
+        VS_OUTPUT output = (VS_OUTPUT)0;
+
+        output.pos = mul(UNITY_MATRIX_MVP, input.vertex);
+        output.uv = input.texcoord;
+
+        return output;
+    }
+
+	fixed4 EncodeLuminance(fixed v)
+	{
+		fixed h = v;
+		fixed positive = v > 0 ? v : 0;
+		fixed negative = v < 0 ? -v : 0;
+
+		fixed4 color = 0;
+
+		color.r = positive;
+		color.g = negative;
+
+		color.ba = frac(color.rg * 256);
+		color.rg -= color.ba / 256.0f;
+		return color;
+	}
+
+	fixed DecodeLuminance(fixed4 v)
+	{
+		fixed4 table = fixed4(1.0f, -1.0f, 1.0f / 256.0f, -1.0f / 256.0f);
+		return dot(v, table);
+	}
+
+	fixed3 ToneMapping(fixed3 oriColor,fixed adaptedLum)
+	{
+		fixed multiplier =  pow(_MiddleGray / adaptedLum, 0.75);
+		return oriColor * multiplier;
+	}
+
+    fixed3 ToneMappingHigh(fixed3 oriColor,fixed adaptedLum)
+	{
+		fixed3 HDRImage = oriColor;
+
+	    fixed multiplier = _MiddleGray / adaptedLum;
+	    fixed p = multiplier > 1 ? 0.5f : 0.85f;
+	    multiplier = pow(multiplier, p);
+	
+	    HDRImage.rgb *= multiplier;
+	
+	    fixed3 table = fixed3(0.3, 0.59, 0.11);
+	    fixed lum = dot(HDRImage.rgb, table.rgb);
+	
+	    fixed final = 1.0f - exp(-lum * _Exposure) * (adaptedLum * _Coeff0);
+    	HDRImage *= final;
+	    return HDRImage;
+	}
+
+	fixed4 LogLuminancePS(VS_OUTPUT input) : COLOR
+	{
+		fixed4 rgba = tex2D(_MainTex,input.uv);
+		fixed luminance = dot(rgba.rgb, fixed3(0.3, 0.59, 0.11));
+		fixed logLuminance = log(luminance + 0.0001);
+		return EncodeLuminance(logLuminance);
+	}
+
+	fixed4 Average4x4SamplesPS(VS_OUTPUT input) : COLOR
+	{
+		fixed lum = 0;
+		for (int i = 0; i < 16; i++)
+		{
+			float2 texcoord = input.uv + g_texOffset4x4[i].xy * _AverImageSizeInverse.xy;
+			lum += DecodeLuminance(tex2D(_MainTex, texcoord));
+		}
+		lum *= 0.0625;
+		return EncodeLuminance(lum);
+    }
+
+	fixed4 Average2x2SamplesPS(VS_OUTPUT input) : COLOR
+	{
+		fixed lum = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			float2 texcoord = input.uv + g_texOffset2x2[i].xy * _AverImageSizeInverse.xy;
+			lum += exp(DecodeLuminance(tex2D(_MainTex, texcoord)));
+		}
+		lum *= 0.25;
+		return EncodeLuminance(lum);
+    }
+
+
+	fixed4 AdaptiveLuminancePS(VS_OUTPUT input) : COLOR
+	{
+		fixed prevLuminance = DecodeLuminance(tex2D(_PreAverLumTex,  half2(0.5, 0.5)));
+		fixed currentLuminance = DecodeLuminance(tex2D(_CurrAverLumTex, half2(0.5, 0.5)));
+		fixed newLuminance = lerp(prevLuminance, currentLuminance, saturate(_AdaptiveSpeed * _ElapsedTime));
+		return EncodeLuminance(newLuminance);
+	}
+
+
+	fixed4 AutoExposurePS(VS_OUTPUT input) : COLOR
+	{
+		fixed4 image = tex2D(_MainTex, input.uv);
+		fixed adaptedLum = (DecodeLuminance(tex2D(_AdaptedLumTex, half2(0.5, 0.5))));
+		fixed3 color = ToneMappingHigh(image.rgb,adaptedLum);
+		return fixed4(color.rgb,1.0);
+
+	}
+
+	fixed4 AutoExposureBloomPS(VS_OUTPUT input) : COLOR
+	{
+		fixed4 image = tex2D(_MainTex, input.uv);
+		fixed4 bloom = tex2D(_BloomTex, half2(input.uv.x,1 - input.uv.y));
+		fixed adaptedLum = (DecodeLuminance(tex2D(_AdaptedLumTex, half2(0.5, 0.5))));
+		fixed3 color = ToneMappingHigh(image.rgb,adaptedLum);
+		return fixed4(color.rgb + bloom.rgb,1.0);
+	}
+
+
+    fixed4 BrightnessPS(VS_OUTPUT input) : COLOR
+    {
+    	fixed4 rgba = tex2D(_MainTex,input.uv);
+    	fixed  adaptedLum = tex2D( _CurrAverLumTex, float2(0.5f, 0.5f) );
+    	rgba.rgb = ToneMappingHigh(rgba.rgb,adaptedLum);
+    	rgba = max(rgba, 0.0f);
+    	fixed4 old_intensity = dot(rgba.rgb, fixed3(0.3, 0.59, 0.11));
+		fixed4 new_intensity = (old_intensity + _BrightOffset) * _BrightScale;
+		return fixed4(rgba.rgb * new_intensity,1.0);
+    }
+    
+    ENDCG
+	
+	SubShader 
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		Lighting Off
+		ZWrite Off
+		AlphaTest Off
+		Pass
+		{
+			Name "Luminance"
+			CGPROGRAM
+
+			#pragma vertex MainVS
+			#pragma fragment LogLuminancePS
+			#pragma target 2.0
+
+			ENDCG
+		}
+
+		Pass
+		{
+			Name "Average4x4Samples"
+			CGPROGRAM
+
+            #pragma vertex MainVS
+            #pragma fragment Average4x4SamplesPS
+            #pragma target 2.0
+           
+            ENDCG
+		}
+
+		Pass
+		{
+			Name "Average2x2amples"
+			CGPROGRAM
+
+            #pragma vertex MainVS
+            #pragma fragment Average2x2SamplesPS
+            #pragma target 2.0
+           
+            ENDCG
+		}
+
+
+		Pass
+		{
+			Name "AdaptiveLuminance"
+			CGPROGRAM
+
+			#pragma vertex MainVS
+			#pragma fragment AdaptiveLuminancePS
+			#pragma target 2.0
+
+			ENDCG
+		}
+
+		Pass
+		{
+			Name "AutoExposureWithoutBloom"
+			CGPROGRAM
+
+            #pragma vertex MainVS
+            #pragma fragment AutoExposurePS
+            #pragma target 2.0
+
+			ENDCG
+		}
+
+		Pass
+		{
+			Name "AutoExposureWithBloom"
+			//Blend one one
+			CGPROGRAM
+
+            #pragma vertex MainVS
+            #pragma fragment AutoExposureBloomPS
+            #pragma target 2.0
+
+			ENDCG
+		}
+
+		Pass
+		{
+			Name "Brightness"
+			CGPROGRAM
+
+            #pragma vertex MainVS
+            #pragma fragment BrightnessPS
+            #pragma target 2.0
+           
+            ENDCG
+		}
+	}
+}
